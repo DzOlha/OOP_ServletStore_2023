e6276fb223131280ad25661021a02500d86cde69
DzOlha <monsarda11@gmail.com>
2023-06-11 20:53:19 +0300

    Created data-access-object classes.

---
+++ src/main/java/dao/UserDao.java
@@ -0,0 +1,24 @@
+package dao;
+
+import entity.User;
+import service.UserService;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface UserDao {
+    void editBlocked(int id, boolean blocked);
+
+    void registerUser(User user);
+
+    UserService.UserInfo findUser(String name, String password);
+
+    User findUserByName(String name);
+    List<User> getClientUsers();
+
+    User getUser(int id);
+
+    User getUserFromResultSet(ResultSet rs) throws SQLException;
+
+}
